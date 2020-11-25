9778562dd8cccb00d8f40128c81c0ecd9dc910fe
tobiashjalmarsson <tobhja@student.chalmers.se>
2020-11-25 08:02:59 +0100

    Added comments
    Added some comments to make code more readable for future use.

--- scripts.js
+++ scripts.js
@@ -3,8 +3,7 @@
 var sorting_container  = document.getElementById("sortingContainer");
 let elements = [];
 var in_progress = false;
-/*sleep(ms) is used in the sorting functions to make them
-more visibly appealing */
+/*delay function which delays ms milliseconds*/
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
@@ -12,17 +11,22 @@ function sleep(ms) {
 // Generate the divs so that we can sort them
 // function to add, take an input value to decide the sice of the array
 function GenerateArray() {
+    //get a reference for the container in which we will create the divs/bars
     let main_node = document.getElementById("sortingContainer");
+    // get input from slider to determine the number of divs/bars
     let number_of_elements = document.getElementById("numElements").value;
-
+    /*In the case that we already have generated bars we want to remove
+    thoose and then generate new bars */
     if(in_progress == true){
         main_node.innerHTML = "";
         in_progress = false;
     }
     in_progress = true;
+    // placeholder array
     let array = [];
     let height = 0;    
     for(let i = 0; i < number_of_elements; i++){
+        // for each element we add we set a certain style, perhaps move this to a seperate function
         height = Math.floor(Math.random()*101) * 5;
         var element = document.createElement("D");
         element.classList.add("sorting_element");
@@ -37,9 +41,12 @@ function GenerateArray() {
 
 // Animation function for when arrays are sorted
 async function finished(){
+    // get all divs in the sortingContainer and store them in all_items
     let all_items = document.getElementById("sortingContainer");
     for(let i = 0; i < all_items.children.length; i++){
+        // change color to indicate that we are done
         all_items.children[i].style.backgroundColor = "green";
+        //delay for 15 ms
         await sleep(15);
     }
 }
@@ -48,13 +55,18 @@ async function finished(){
 // Sorting functions
 
 async function insertion_sort(){
+    // get the input from the slider which determines the speed
     let speed = document.getElementById("speed").value;
-    speed = 200 - speed;
+    // get a reference for the container of our divs.
+    // perhaps change this to document.getElementById("sortingContainer").children to make code cleaner
     let all_items = document.getElementById("sortingContainer");
     const low = 0;
+    // get the number of divs of our container
     const high = all_items.children.length - 1;
+    //sorting
     for(let i = low + 1; i <= high; i++){
         let value = all_items.children[i];
+        // set style to blue to indicate that it's being moved
         value.style.backgroundColor = "blue";
         await sleep(speed);
         let j = i;
@@ -64,6 +76,7 @@ async function insertion_sort(){
             j--;
         }
         all_items.insertBefore(all_items.children[j], value);
+        // when it's in place, change color to greenyellow
         all_items.children[j+1].style.backgroundColor = "greenyellow";
         value.style.backgroundColor = "greenyellow";
     }
